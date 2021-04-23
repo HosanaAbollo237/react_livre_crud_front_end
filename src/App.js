b4b2f4dcb3b4f68b049161a73f20949679f0038e
HosanaAbollo237 <hosana.abollo237@gmail.com>
2021-04-23 20:40:57 +0200

    init project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class App extends Component {
+  render(){
+    return(
+      <>
+        <h1>Titre</h1>
+        <div>Livres</div>
+        <button>Ajouter</button>
+      </>
+    )
+  }
+}
+export default App;
